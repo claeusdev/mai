59255e4244df5bbb93b4ad04a756436d76c85677
Nana Adjei Manu <claeusdev@gmail.com>
2016-06-06 02:52:57 +0000

    Added name n gender to users n added confirmable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
+  config.action_mailer.perform_deliveries = true
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605055150) do
+ActiveRecord::Schema.define(version: 20160606010130) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -49,6 +49,11 @@ ActiveRecord::Schema.define(version: 20160605055150) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "name"
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
