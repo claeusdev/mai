889896b0816322728ca47fd738e24370852da7cc
Nana Adjei Manu <claeusdev@gmail.com>
2016-06-12 06:20:04 +0000

    Changed devise path after sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  def after_sign_in_path_for(resource)
-    user_path(current_user) #your path
-  end
+
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -18,5 +16,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :image, :gender, :last_name, :username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :image, :gender, :last_name, :username])
 
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user) #your path
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user) #your path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
   resources :categories
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :users
 
   get 'pages/home'
