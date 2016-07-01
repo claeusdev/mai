5aef9d2db00ad11a377d135a462a84d1fe553929
Nana Adjei Manu <claeusdev@gmail.com>
2016-07-01 15:52:40 +0000

    Store id fix for production

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -11,7 +11,6 @@ class StoresController < ApplicationController
   # GET /stores/1
   # GET /stores/1.json
   def show
-    @stores = Store.find(params[:id])
   end
 
   def search
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    # @stores = Store.find(params[:id])
+    @stores = Store.find(params[:id])
   end
 
   def index
