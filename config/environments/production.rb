6c3ee322dc9438c62228bb34f216d47890174b36
Nana Adjei Manu <claeusdev@gmail.com>
2016-07-18 23:03:08 +0000

    Fixed sendgrid issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,6 @@ Rails.application.configure do
   config.assets.compile = true
 
 
-  config.action_mailer.default_url_options = {  "sleepy-gorge-32201.herokuapp.com/"}
   config.action_mailer.default_url_options = { host: 'https://quiet-refuge-15852.heroku.com' }
    config.action_mailer.delivery_method = :smtp
 
