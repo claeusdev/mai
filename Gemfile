b54df4a247a3c9ad52d5fd512cb9795137c699a7
Nana Adjei Manu <claeusdev@outlook.com>
2017-01-04 01:53:24 +0000

    version 2.0 start

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
-ruby '2.3.0'
+ruby '2.3.1'
 
 
-gem 'rails', '4.2.6'
+gem 'rails', '5.0.1'
 
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
@@ -19,7 +19,7 @@ gem 'mail_form', '~> 1.5', '>= 1.5.1'
 gem "paperclip", git: "git://github.com/thoughtbot/paperclip.git"
 gem 'simple_form'
 gem 'rmagick', '~> 2.15', '>= 2.15.4'
-gem 'administrate'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -38,9 +37,8 @@ ActiveRecord::Schema.define(version: 20160710010648) do
     t.integer  "user_id"
     t.integer  "subcategory_id"
     t.integer  "store_id"
+    t.index ["category_id"], name: "index_listings_on_category_id"
   end
-
-  add_index "listings", ["category_id"], name: "index_listings_on_category_id"
 
   create_table "orders", force: :cascade do |t|
     t.string   "address"
@@ -101,10 +99,9 @@ ActiveRecord::Schema.define(version: 20160710010648) do
     t.text     "about"
     t.string   "city"
     t.string   "country"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
